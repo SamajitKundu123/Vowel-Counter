9ee3aef49db0b68de63ba312d173bf46f2c02e1c
Samajit Kundu <126185958+SamajitKundu123@users.noreply.github.com>
2024-02-21 21:21:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function countVowel(){
+
+    var text = document.getElementById("inputText").value;
+    var vowelCount = 0;
+
+    text = text.toLowerCase();
+
+    for(var i=0; i < text.length; i++ ){
+        var char = text.charAt(i);
+        if(isVowel(char)){
+            vowelCount++;
+        }
+    }
+
+    var result = document.getElementById("result");
+    result.textContent = "total Vowels:" + vowelCount;
+
+}
+
+function isVowel(char){
+
+    var vowels = ["a","e","i","o","u"];
+    return vowels.includes(char);
+}
